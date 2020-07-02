fd748810b91a6532772ecf9e7b5faf1b576494f1
Journey Cruz <journey.cruz@gmail.com>
2020-07-02 12:01:09 -0500

    functionality for creating new notes and recieving notes previously made

--- server.js
+++ server.js
@@ -11,3 +11,35 @@ app.use(express.json());
 // CREATE A PATH TO PUBLIC FOLDER THAT HOLDS ASSETS AND HTML FILES //
 app.use(express.static(path.join(__dirname, "public")));
 
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "db/db.json"));
+});
+
+
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+
+    fs.readFile("db/db.json", (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        notes.push(newNote);
+
+        fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            console.log("Successfully wrote file");
+            return res.json(newNote);
+        });
+    });
+});
+
